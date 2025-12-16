d5ee09c3c9c478b8141a6f2a02a6d1788188d032
Paul Dziedzic <53009545+pauld4@users.noreply.github.com>
2025-12-16 17:57:48 -0500

    Delete login.e2e.js

--- login.e2e.js
+++
@@ -1,8 +0,0 @@
-describe('Google search', () => {
-  it('should open Google and check title', async () => {
-    await browser.url('https://www.google.com')
-
-    const title = await browser.getTitle()
-    expect(title).toContain('Google')
-  })
-})
